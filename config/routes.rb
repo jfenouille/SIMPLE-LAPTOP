9fdc6198a2bfcce7a964f14b1fc8511395754bda
denny juliette <dny1803@gmail.com>
2023-05-24 17:55:34 +0400

    corrected user

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  # devise_for :users
+  devise_for :users
   root to: "pages#home"
-  resources :user do
-    resources :products
-    resources :bookings
-  end
-  resources :products, only: :destroy
-  resources :bookings, only: :destroy
+  # resources :users do
+  #   resources :products
+  #   resources :bookings
+  # end
+  # resources :products, only: :destroy
+  # resources :bookings, only: :destroy
 end
