e28540544f3cf980573eaab42bac92779a1c1e42
Jullien Fenouille <jullien.fenouille@outlook.com>
2023-05-27 12:48:20 +0400

    new booking

--- db/schema.rb
+++ db/schema.rb
@@ -47,7 +47,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_05_26_170040) do
     t.datetime "updated_at", null: false
     t.string "first_name"
     t.string "last_name"
-    t.boolean "renter"
+    t.boolean "renter", default: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
