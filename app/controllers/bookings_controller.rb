4bbabb0674cfcd410cce6b2c243954dd8d43fd54
denny juliette <dny1803@gmail.com>
2023-05-29 07:25:06 +0400

    many changes

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,11 +1,12 @@
 class BookingsController < ApplicationController
-  before_action :set_booking, only: [ :show, :destroy]
+  before_action :set_booking, only: [ :show, :destroy, :update, :edit]
   # test
   def index
     @bookings = Booking.all
   end
 
   def show
+    @product = @booking.product
   end
 
   def new
@@ -23,6 +24,18 @@ class BookingsController < ApplicationController
     end
   end
 
+  def edit
+    @product = @booking.product
+  end
+
+  def update
+    if @booking.update(booking_params)
+      redirect_to booking_path(@booking)
+    else
+      render :new
+    end
+  end
+
   def destroy
     @booking.destroy
     redirect_to bookings_path, status: :see_other
@@ -35,6 +48,6 @@ class BookingsController < ApplicationController
   end
 
   def booking_params
-    params.require(:booking).permit(:payment, :days)
+    params.require(:booking).permit(:payment, :days, :product_id, :user_id)
   end
 end
