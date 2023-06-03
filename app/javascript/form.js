af8b5cf6cdccec67ff25bef8d3c4d80327bcec5f
denny juliette <dny1803@gmail.com>
2023-06-03 12:06:01 +0400

    modified footer

--- app/javascript/form.js
+++ app/javascript/form.js
@@ -1,9 +1,13 @@
-
-  document.addEventListener('DOMContentLoaded', function() {
-    const confirmBtn = document.querySelector('.confirm-btn');
-    const spinner = document.querySelector('.spinner-border');
+/* 
+  window.addEventListener('DOMContentLoaded', function() {
+    let confirmBtn = document.querySelector('.confirm-btn');
+    let spinner = document.querySelector('.spinner-border');
 
     confirmBtn.addEventListener('click', function() {
       spinner.classList.remove('d-none');
     });
-  });
+  }); */
+
+  function square() {
+    spinner.classList.remove('d-none');
+  }
