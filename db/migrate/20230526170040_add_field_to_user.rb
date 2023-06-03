593bc3466f9922db0268c7642b40fbdeb2ccdb3a
Jullien Fenouille <jullien.fenouille@outlook.com>
2023-06-03 14:26:05 +0400

    user modified

--- db/migrate/20230526170040_add_field_to_user.rb
+++ db/migrate/20230526170040_add_field_to_user.rb
@@ -1,5 +1,5 @@
 class AddFieldToUser < ActiveRecord::Migration[7.0]
   def change
-    add_column :users, :renter, :boolean, default: false
+    add_column :users, :renter, :boolean, default: true
   end
 end
